ef82906f4438e619a6766bcbdb9beae34abcff5f
ManuFinn <Manu_Finn117@outlook.com>
2022-04-18 21:36:05 -0500

    Agregar archivos de proyecto.

---
+++ FastNotesAPI/Models/Notas/Fastnotesapi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FastNotesAPI.Models.Notas
+{
+    public partial class Fastnotesapi
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public ulong Eliminado { get; set; }
+        public DateTime? Timestamp { get; set; }
+    }
+}
---
+++ FastNotesAPI/Models/Notas/progmovilContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace FastNotesAPI.Models.Notas
+{
+    public partial class progmovilContext : DbContext
+    {
+        public progmovilContext()
+        {
+        }
+
+        public progmovilContext(DbContextOptions<progmovilContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Fastnotesapi> Fastnotesapi { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Fastnotesapi>(entity =>
+            {
+                entity.ToTable("fastnotesapi");
+
+                entity.Property(e => e.Contenido)
+                    .IsRequired()
+                    .HasMaxLength(1024);
+
+                entity.Property(e => e.Eliminado).HasColumnType("bit(1)");
+
+                entity.Property(e => e.Timestamp).HasColumnType("datetime");
+
+                entity.Property(e => e.Titulo).HasMaxLength(45);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
