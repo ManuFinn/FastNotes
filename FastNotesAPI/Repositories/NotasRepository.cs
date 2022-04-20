4f456b4614b99c302038c7de4a64388a9b3a66ae
ManuFinn <Manu_Finn117@outlook.com>
2022-04-19 21:33:56 -0500

    La clase "NotasRepository" creada y terminada.

---
+++ FastNotesAPI/Repositories/NotasRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using FastNotesAPI.Models.Notas;
+
+namespace FastNotesAPI.Repositories
+{
+    public class NotasRepository : Repository<Fastnotesapi>
+    {
+        public NotasRepository(DbContext context) : base(context)
+        {
+        }
+
+        public override void Insert(Fastnotesapi entity) //Pendiente checar el DateTime para acomodarlo a la hora mexico
+        {
+            entity.Timestamp = DateTime.UtcNow;
+            base.Insert(entity);
+        }
+
+        public override IEnumerable<Fastnotesapi> GetAll()
+        {
+            return base.GetAll().Where(x => x.Eliminado == 0).OrderBy(x => x.Timestamp);
+        }
+
+        public IEnumerable<Fastnotesapi> GetAllSinceDate(DateTime timestamp)
+        {
+            var cambiados = base.GetAll().Where((x) => x.Eliminado == 0 && x.Timestamp > timestamp);
+            var eliminados = base.GetAll().Where((x) => x.Eliminado == 1 && x.Timestamp > timestamp)
+                .Select(x => new Fastnotesapi { Id = x.Id });
+
+            return cambiados.Concat(eliminados);
+        }
+
+        public override void Update(Fastnotesapi entity)
+        {
+            entity.Timestamp = DateTime.UtcNow;
+            base.Update(entity);
+        }
+
+        public override void Delete(Fastnotesapi entity)
+        {
+            if(entity.Eliminado == 0)
+            {
+                entity.Timestamp = DateTime.UtcNow;
+                entity.Eliminado = 1;
+                base.Update(entity);
+            }
+
+            int TTL = 64;
+
+            var fechaEliminar = DateTime.UtcNow.Subtract(TimeSpan.FromDays(TTL));
+
+            var porEliminar = base.GetAll().Where(x=> x.Eliminado == 1 && x.Timestamp < fechaEliminar);
+
+            foreach (var item in porEliminar)
+            {
+                Context.Remove(item);
+            }
+            Context.SaveChanges();
+        }
+
+        public override bool IsValid(Fastnotesapi entity, out List<string> validationErrors)
+        {
+            validationErrors = new List<string>();
+
+            if(string.IsNullOrWhiteSpace(entity.Contenido))
+            {
+                validationErrors.Add("El contenido de la nota se encuentra vacia");
+            }
+            return validationErrors.Count == 0;
+        }
+
+
+    }
+}
