31e396143689ff1489bf49a1fb9b191a1fe12d37
ManuFinn <Manu_Finn117@outlook.com>
2022-06-16 20:07:38 -0500

    Icono y nombre cambiado a la aplicacion de Notas

--- MyNotes/MyNotes.Android/MainActivity.cs
+++ MyNotes/MyNotes.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace MyNotes.Droid
 {
-    [Activity(Label = "MyNotes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Notes io", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
